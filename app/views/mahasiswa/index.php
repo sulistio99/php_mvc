5d0b6116204211935cf8370999e2e7fbb100d96f
sulistio99 <74336534+sulistio99@users.noreply.github.com>
2020-11-23 22:02:20 +0800

    Tambah SEARCHING

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -6,13 +6,32 @@
     </div>
   </div>
 
-  <div class="row">
+  <div class="row mb-3">
     <div class="col-lg-6">
-      <!-- Button trigger modal -->
       <button type="button" class="btn btn-primary tomnolTambahData" data-toggle="modal" data-target="#formModal">
         Tambah Data Mahasiswa
       </button>
-      <br>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-lg-6">
+      <form action="<?= BASEURL; ?>/mahasiswa/cari" method="POST">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Masukkan Pencarian..." aria-describedby="button-addon2" name="keyword" id="keyword" autocomplete="off">
+          <div class="input-group-append">
+            <button class="btn btn-primary" type="submit" id="tombolCari">Cari</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+
+  <div class="row">
+    <div class="col-lg-6">
+      <!-- Button trigger modal -->
+
       <h3>Daftar Mahasiswa</h3>
       <ul class="list-group">
         <?php foreach ($data['mhs'] as $mhs) : ?>
